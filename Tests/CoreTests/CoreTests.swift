08a3c19bdbf144de5bd4b1f592bb2e308e77572b
kulikov-ivan <kulikovia.72@icloud.com>
2021-05-21 23:17:36 +0300

    cleanup

--- Tests/CoreTests/CoreTests.swift
+++ Tests/CoreTests/CoreTests.swift
@@ -23,16 +23,51 @@ private var fullManifestWithFullPackagefile: Manifest?
 
 final class CoreTests: XCTestCase {
 
-    func test01_emptyPackagefileParsing() { testPackagefileParsing(at: emptyPackagefilePath, saveTo: &emptyPackagefile) }
-    func test02_emptyPackagefileComparing() { testPackagefilesComparing(emptyPackagefile, Self.expectedEmptyPackagefile) }
-    func test03_fullPackagefileParsing() { testPackagefileParsing(at: fullPackagefilePath, saveTo: &fullPackagefile) }
-    func test04_fullPackagefileComparing() { testPackagefilesComparing(fullPackagefile, Self.expectedFullPackagefile) }
-    func test05_emptyManifestWithEmptyPackagefileParsing() { testManifestParsing(at: emptyManifestPath, with: emptyPackagefile, saveTo: &emptyManifestWithEmptyPackagefile) }
-    func test06_emptyManifestWithEmptyPackagefileComparing() { testManifestsComparing(emptyManifestWithEmptyPackagefile, Self.expectedEmptyManifestWithEmptyPackagefile) }
-    func test07_emptyManifestWithFullPackagefileParsing() { testManifestParsing(at: emptyManifestPath, with: fullPackagefile, saveTo: &emptyManifestWithFullPackagefile) }
-    func test08_emptyManifestWithFullPackagefileComparing() { testManifestsComparing(emptyManifestWithFullPackagefile, Self.expectedEmptyManifestWithFullPackagefile) }
-    func test09_fullManifestWithEmptyPackagefileParsing() { testManifestParsing(at: fullManifestPath, with: emptyPackagefile, saveTo: &fullManifestWithEmptyPackagefile) }
-    func test10_fullManifestWithEmptyPackagefileComparing() { testManifestsComparing(fullManifestWithEmptyPackagefile, Self.expectedFullManifestWithEmptyPackagefile) }
-    func test11_fullManifestWithFullPackagefileParsing() { testManifestParsing(at: fullManifestPath, with: fullPackagefile, saveTo: &fullManifestWithFullPackagefile) }
-    func test12_fullManifestWithFullPackagefileComparing() { testManifestsComparing(fullManifestWithFullPackagefile, Self.expectedFullManifestWithFullPackagefile) }
+    func test01_emptyPackagefileParsing() {
+        testPackagefileParsing(at: emptyPackagefilePath, saveTo: &emptyPackagefile)
+    }
+
+    func test02_emptyPackagefileComparing() {
+        testPackagefilesComparing(emptyPackagefile, Self.expectedEmptyPackagefile)
+    }
+
+    func test03_fullPackagefileParsing() {
+        testPackagefileParsing(at: fullPackagefilePath, saveTo: &fullPackagefile)
+    }
+
+    func test04_fullPackagefileComparing() {
+        testPackagefilesComparing(fullPackagefile, Self.expectedFullPackagefile)
+    }
+
+    func test05_emptyManifestWithEmptyPackagefileParsing() {
+        testManifestParsing(at: emptyManifestPath, with: emptyPackagefile, saveTo: &emptyManifestWithEmptyPackagefile)
+    }
+
+    func test06_emptyManifestWithEmptyPackagefileComparing() {
+        testManifestsComparing(emptyManifestWithEmptyPackagefile, Self.expectedEmptyManifestWithEmptyPackagefile)
+    }
+
+    func test07_emptyManifestWithFullPackagefileParsing() {
+        testManifestParsing(at: emptyManifestPath, with: fullPackagefile, saveTo: &emptyManifestWithFullPackagefile)
+    }
+
+    func test08_emptyManifestWithFullPackagefileComparing() {
+        testManifestsComparing(emptyManifestWithFullPackagefile, Self.expectedEmptyManifestWithFullPackagefile)
+    }
+
+    func test09_fullManifestWithEmptyPackagefileParsing() {
+        testManifestParsing(at: fullManifestPath, with: emptyPackagefile, saveTo: &fullManifestWithEmptyPackagefile)
+    }
+
+    func test10_fullManifestWithEmptyPackagefileComparing() {
+        testManifestsComparing(fullManifestWithEmptyPackagefile, Self.expectedFullManifestWithEmptyPackagefile)
+    }
+
+    func test11_fullManifestWithFullPackagefileParsing() {
+        testManifestParsing(at: fullManifestPath, with: fullPackagefile, saveTo: &fullManifestWithFullPackagefile)
+    }
+
+    func test12_fullManifestWithFullPackagefileComparing() {
+        testManifestsComparing(fullManifestWithFullPackagefile, Self.expectedFullManifestWithFullPackagefile)
+    }
 }
